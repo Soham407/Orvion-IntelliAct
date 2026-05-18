c1d0d4b0f1310d4f7a3b58c8b5ef31cc192d4074
Soham407 <sohambhutkar1@gmail.com>
2026-05-18 10:06:46 +0530

    Update legacy navbar scroll background

--- components/site-header.js
+++ components/site-header.js
@@ -24,6 +24,8 @@ export function SiteHeader() {
     const handleScroll = () => {
       setScrolled(window.scrollY > 20);
     };
+
+    handleScroll();
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
